b1d3a4c7b4c751a44e01300135994dca8032b9f3
Tom Machado <tmachad3@gmail.com>
2019-10-04 19:54:24 -0400

    Added floors and basic player movement

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public GameObject floorPrefab;
+    public GameObject letterPrefab;
+
+    private Transform levelRoot;
+
+    private void Awake()
+    {
+        levelRoot = transform;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float stepDelay = 1.0f;
+
+    public LayerMask walkableLayers;
+
+    private float remainingDelay = 0.0f;
+
+    private void Update()
+    {
+        Vector2 input = new Vector2(
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical")
+        );
+
+        if (remainingDelay <= 0 && (input.x != 0 || input.y != 0))
+        {
+            Move((int)input.x, (int)(input.x == 0 ? input.y : 0));
+            remainingDelay = stepDelay;
+        } else if (input.x == 0 && input.y == 0)
+        {
+            remainingDelay = 0; // Instantly reset delay if player stops pressing keys to allow for fast movement 
+        } else
+        {
+            remainingDelay -= Time.deltaTime;
+        }
+    }
+
+    private void Move(int x, int y)
+    {
+        Vector3 pos = transform.position;
+        pos.x += x;
+        pos.z += y;
+
+        if (Physics.Raycast(pos, Vector3.down, 1.0f, walkableLayers))
+        {
+            // Walkable surface detected in the next spot, so just move there
+            transform.position = pos;
+        } else
+        {
+            // No walkable surface detected, play some sort of animation to notify the player
+        }
+    }
+}
